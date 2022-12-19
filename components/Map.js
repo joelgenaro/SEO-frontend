2bd56dc8c04c74e5868853b2f3f30af34045ed80
DESKTOP-O9A3ER0\Rsj <seniordeveloper754@gmail.com>
2022-12-19 04:22:08 -0500

    operator

--- components/Map.js
+++ components/Map.js
@@ -13,8 +13,12 @@ const Map = ({ companies }) => {
     if (obj["Company_Location_Geo"]) {
       let coordinate = obj["Company_Location_Geo"].split(",");
 
-      let lat = Number(coordinate[0]?.slug.replace('"', ""));
-      let lng = Number(coordinate[1]?.slug.replace('"', ""));
+      let lat = Number(
+        coordinate[0] ? coordinate[0].slug.replace('"', "") : ""
+      );
+      let lng = Number(
+        coordinate[1] ? coordinate[1].slug.replace('"', "") : ""
+      );
 
       return { id: key, position: { lat: lat, lng: lng } };
     } else if (obj["Company_Location_Name"] != "") {
